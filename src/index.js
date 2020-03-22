d85fae79c10ded0e1a2071fa1e4e835d0d68d249
Tatsiana Alsheuskaya <ttttanna@gmail.com>
2020-03-22 18:11:45 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  
+  for (let item of bracketsConfig) {
+    arr.push(item.join(""));
+  }
+
+  for (let i = 0; i< arr.length;) {
+    if (str.includes(arr[i])) {
+      str = str.replace(arr[i], "");
+      i = 0;
+    }
+    else i++;
+  }
+
+  if (str.length > 0) {
+    return false;
+  } else return true;
+
+
+  
+
 }
